936f181507eb7ccb0ff17d3ed3e3db3da4ba18a7
Abdullah Ishtiaq <abdullahishtiaq335@gmail.com>
2025-02-20 00:13:09 -0800

    Chapter 1 Exercise Complete

---
+++ Chapter_1/script.js
@@ -0,0 +1,13 @@
+// Question 1
+
+alert ("Hello Welcome to SMIT");
+
+// Question 2
+
+alert ("Error! Please enter a valid password.");
+
+// Question 3
+
+var alert_3 = ("Welcome to JS Land... \nHappy Coding!").toUpperCase() .trim();
+alert (alert_3);
+
