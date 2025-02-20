a05e9ab10d1677fc0bcc8945fa0322872c30b8fc
Abdullah Ishtiaq <abdullahishtiaq335@gmail.com>
2025-02-20 00:36:15 -0800

    Chapter 2 Exercise Complete

---
+++ Chapter_2/script.js
@@ -0,0 +1,25 @@
+var username ;
+var myName = "Abdullah Ishtiaq";
+var message = "Hello World";
+
+userName = "Abdullah Ishtiaq";
+var age = 15;
+var certificate = "Certified Moblie app Development";
+
+alert ("Student name: " + userName );
+alert ("Student Age: " + age);
+alert ("Student Certification: " + certificate);
+
+var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+alert (pizza);
+
+var email = "abdullahishtiaq335@gmail.com";
+alert ("My Email Addres is : " + email);
+
+var book = "A smarter way to learn JavaScript";
+alert ("I am learning from : " + book + " from this Book");
+
+document.write ("Yeah ! I can write HTML content through JavaScript");
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert (design);
