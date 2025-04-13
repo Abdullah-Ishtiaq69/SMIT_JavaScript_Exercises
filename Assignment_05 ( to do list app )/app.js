ef2b6fe23416d4a466ad1b218a078aea720b87fc
Abdullah Ishtiaq <abdullahishtiaq335@gmail.com>
2025-04-13 12:19:29 -0700

    24 Questions Solved

---
+++ Assignment_05 ( to do list app )/app.js
@@ -0,0 +1,5 @@
+// console.dir(document);
+
+// let h1 = document.querySelector('h1');
+// console.log(h1.innerHTML);
+// console.log(h1.inner
--- Solutions/Chapter_1_to_5/app.js
+++ Solutions/Chapter_1_to_5/app.js
@@ -108,3 +108,113 @@
 // var num2 = +prompt('Enter a number');
 // var div = num1 / num2;
 // alert('Division of ' + num1 + ' and ' + num2 + ' is ' + div);
+
+// Question 19
+
+// var calc;
+// var output1 = document.getElementById('p1');
+// var output2 = document.getElementById('p2');
+// var output3 = document.getElementById('p3');
+// var output4 = document.getElementById('p4');
+// var output5 = document.getElementById('p5');
+// var output6 = document.getElementById('p6');
+// var output7;
+// output1.innerHTML = ('Value after variable declaration is: ' + calc + '<br />');
+// calc = 2;
+// output2.innerHTML = ('Initial value: ' + calc + '<br />');
+// calc++
+// output3.innerHTML = ('Value after Increment is: ' + calc + '<br />');
+// calc += 7;
+// output4.innerHTML = ('After addition is: ' + calc + '<br />');
+// calc--
+// output5.innerHTML = ('Value after Decrement is: ' + calc + '<br />');
+// output7 = calc % 3
+// output6.innerHTML = ('The reminder is : ' + output7);
+
+// Question 20
+
+// var ticketPrice = 600;
+// var ticketQuantity = +prompt('Enter your ticket quantity');
+// var totalPrice = ticketPrice * ticketQuantity;
+
+// var heading = document.getElementById('heading');
+// var heading2Price = document.getElementById('price');
+// var heading2Quantity = document.getElementById('quantity');
+// var totalHeading = document.getElementById('total');
+
+// heading.innerHTML = ('Movie Tickets <br />');
+// heading2Price.innerHTML = ('Each Ticket Price is ' + ticketPrice + '<br />');
+// heading2Quantity.innerHTML = ('Your Ticket Quantity is ' + ticketQuantity + '<br />');
+// totalHeading.innerText = ('Total cost to buy ' + ticketQuantity + ' tickets to a movie is ' + totalPrice + ' pkr. ' );
+
+// Question 21
+
+// var numberTable = +prompt('Enter a number to generate its table');
+// var startingNumber = 1;
+// console.log(numberTable + ' x ' + startingNumber + ' = ' + numberTable * startingNumber++);
+// console.log(numberTable + ' x ' + startingNumber + ' = ' + numberTable * startingNumber++);
+// console.log(numberTable + ' x ' + startingNumber + ' = ' + numberTable * startingNumber++);
+// console.log(numberTable + ' x ' + startingNumber + ' = ' + numberTable * startingNumber++);
+// console.log(numberTable + ' x ' + startingNumber + ' = ' + numberTable * startingNumber++);
+// console.log(numberTable + ' x ' + startingNumber + ' = ' + numberTable * startingNumber++);
+// console.log(numberTable + ' x ' + startingNumber + ' = ' + numberTable * startingNumber++);
+// console.log(numberTable + ' x ' + startingNumber + ' = ' + numberTable * startingNumber++);
+// console.log(numberTable + ' x ' + startingNumber + ' = ' + numberTable * startingNumber++);
+// console.log(numberTable + ' x ' + startingNumber + ' = ' + numberTable * startingNumber++);
+
+// Question 22
+
+// var c = 25;
+// var f = 70;
+// var changeC = f + 'F is ' + (f - 32) * 5 / 9 + 'C';
+// var changeF = c + 'C is ' + ((c * 9 / 5) + 32) + 'F';
+// console.log(changeC);
+// console.log(changeF);
+
+// Question 23
+
+// var heading = document.getElementById('heading');
+// var p1 = document.getElementById('p1');
+// var p2 = document.getElementById('p2');
+// var p3 = document.getElementById('p3');
+// var p4 = document.getElementById('p4');
+// var p5 = document.getElementById('p5');
+// var result = document.getElementById('finalP');
+
+// var price1 = +prompt('Enter your first product price');
+// var price2 = +prompt('Enter your second product price');
+// var quantity1 = +prompt('Enter your first product');
+// var quantity2 = +prompt('Enter your second product');
+// var charges = 100;
+// var totalCost = (price1 * quantity1) + (price2 * quantity2) + charges;
+
+// heading.innerHTML = ('Shopping Cart <br /> <br />');
+// p1.innerHTML = ('Price of item 1 is ' + price1 + '<br />');
+// p2.innerHTML = ('Price of item 2 is ' + price2 + '<br />');
+// p3.innerHTML = ('Quantity of item 1 is ' + quantity1 + '<br />');
+// p4.innerHTML = ('Quantity of item 2 is ' + quantity2 + '<br />');
+// p5.innerHTML = ('Shipping Charges is ' + charges + '<br />');
+// result.innerHTML = ('Total Cost of your order is ' + totalCost);
+
+// Question 24
+
+// var totalMarks = 600;
+// var obtainMarks = +prompt('Enter your obtain marks between 1 to 600');
+// var percentage = obtainMarks / totalMarks * 100;
+
+// var h1 = document.getElementById('heading');
+// var p1 = document.getElementById('p1');
+// var p2 = document.getElementById('p2');
+// var final = document.getElementById('p3');
+
+// h1.innerHTML = ('Marks Sheet <br /> <br />');
+// p1.innerHTML = ('Total Marks: ' + totalMarks + '<br />');
+// p2.innerHTML = ('Marks Obtained: ' + obtainMarks + '<br />');
+// final.innerHTML = ('Percentage: ' + percentage + '%  <br />');
+
+// if (obtainMarks > totalMarks) {
+//     final.innerHTML = ('Please Enter a Number between 1 to 600')
+// };
+
+// Question 25
+
