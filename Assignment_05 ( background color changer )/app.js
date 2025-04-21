4e10ee68ae2c39f89e29aaf90db43ede755ed1ba
Abdullah Ishtiaq <abdullahishtiaq335@gmail.com>
2025-04-21 01:53:48 -0700

    Background Color Changer update

--- Assignment_05 ( background color changer )/app.js
+++ Assignment_05 ( background color changer )/app.js
@@ -1,4 +1,5 @@
 button = document.getElementById('btn');
+text = document.getElementById('text');
 
 function randomColor() {
     return Math.floor((Math.random() * 255));
@@ -6,4 +7,5 @@ function randomColor() {
 
 button.addEventListener('click' , () => {
     document.body.style.backgroundColor = `rgb(${randomColor()} , ${randomColor()} , ${randomColor()})`
+    text.innerHTML = `Background Color rgb(${randomColor()} , ${randomColor()} , ${randomColor()})`
 });
