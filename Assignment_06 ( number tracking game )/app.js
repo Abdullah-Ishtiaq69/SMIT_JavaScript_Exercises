8efa3bba777f62f071070d4faee8ae4415352b88
Abdullah Ishtiaq <abdullahishtiaq335@gmail.com>
2025-04-22 11:25:25 -0700

    Number Tracking Game Completed

--- Assignment_06 ( number tracking game )/app.js
+++ Assignment_06 ( number tracking game )/app.js
@@ -1,2 +1,25 @@
-var minimumNumber = 1;
-var maximumNumber = 100;
+var randomNum = Math.floor((Math.random() * 100));
+var input = document.getElementById('input');
+
+var okButton = document.getElementById('first');
+var resetButton = document.getElementById('second');
+
+var result = document.getElementById('result');
+
+okButton.addEventListener('click', () => {
+    if (input.value === randomNum + '') {
+        result.innerHTML = `Congratulations Number Matched ❤️`
+    } else if (input.value === '') {
+        alert('Please Enter a Number Between 1 to 100')
+    } else if (input.value > randomNum) {
+        result.innerHTML = `Sorry You Entered Bigger Number 😑`
+    } else (input.value < randomNum) {
+        result.innerHTML = `Sorry You Entered Smaller Number 😑`
+    }
+});
+
+resetButton.addEventListener('click', () => {
+    input.value = ''
+});
+
+console.log(randomNum);
