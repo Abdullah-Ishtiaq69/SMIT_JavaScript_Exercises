d1d68f862f52079a05307d5b31fc29770f709e13
Abdullah Ishtiaq <abdullahishtiaq335@gmail.com>
2025-04-28 03:22:59 -0700

    Half Completed

--- Assignment_08 ( to do list app )/app.js
+++ Assignment_08 ( to do list app )/app.js
@@ -1,5 +1,38 @@
-// console.dir(document);
+inputAdd = document.getElementById('input');
+ul = document.getElementById('ul');
+form = document.getElementById('form');
+mainBox = document.getElementById('main');
+tasks = document.getElementById('tasks');
 
-// let h1 = document.querySelector('h1');
-// console.log(h1.innerHTML);
-// console.log(h1.inner
+// Add
+
+form.addEventListener('submit' , (e) => {
+    if(inputAdd.value !== ''){
+        e.preventDefault();
+        const li = document.createElement('li');
+        li.innerHTML = `${inputAdd.value.trim()}
+        <span id="edit"><i class="fa-solid fa-pen"></i></span>
+        <span id="delete"><i class="fa-solid fa-trash"></i></span>
+        `;
+        ul.appendChild(li);
+        inputAdd.value = '';
+        inputAdd.focus()
+        textUpdate()
+    } else {
+        alert('Please Enter Some Value');
+    };
+});
+
+// Delete
+
+ul.addEventListener('click' , (e) => {
+    if (e.target.classList.contains('fa-trash')) {
+        e.target.closest('li').remove();
+        textUpdate()
+    }
+});
+
+const textUpdate = () => {
+   const text = mainBox.querySelectorAll('li').length;
+    tasks.textContent = `Total Tasks is: ${text}`;
+}
