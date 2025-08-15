131a64a97226bc6a5095a7d7e409cc57c51fe3c3
abdullah ishtiaq <abdullahishtiaq335@gmail.com>
2025-08-15 10:11:38 -0700

    call , apply , bind Done

--- JavaScript Practices/app.js
+++ JavaScript Practices/app.js
@@ -165,3 +165,78 @@
 
 // console.log(userOne);
 // console.log(userTwo);
+
+// ---------------------------------------------------------------------
+
+// --------------------------- call ---------------------------
+
+// const userOne = {
+//     name: "abdullah",
+//     email: "abdullah@gmail.com",
+//     login() {
+//         console.log(`${this.name} has logged in`);
+//     },
+//     logout() {
+//         console.log(`${this.name} has logged out`);
+//     }
+// }
+
+// const userTwo = {
+//     name: "ali",
+//     email: "ali@gmail.com"
+// }
+
+// userOne.login.call(userTwo);
+
+// --------------------------- apply ---------------------------
+
+// const userOne = {
+//     name: "abdullah",
+//     email: "abdullah@gmail.com",
+//     login() {
+//         console.log(`${this.name} has logged in`);
+//     },
+//     logout() {
+//         console.log(`${this.name} has logged out`);
+//     }
+// }
+
+// const userTwo = {
+//     name: "ali",
+//     email: "ali@gmail.com"
+// }
+
+// userOne.login.apply(userTwo);
+
+// --------------------------- bind ---------------------------
+
+// const userOne = {
+//     name: "abdullah",
+//     email: "abdullah@gmail.com",
+//     login() {
+//         console.log(`${this.name} has logged in`);
+//     },
+//     logout() {
+//         console.log(`${this.name} has logged out`);
+//     }
+// }
+
+// const userTwo = {
+//     name: "ali",
+//     email: "ali@gmail.com"
+// }
+
+// let userTwoBinded = userOne.login.bind(userTwo)
+// userTwoBinded()
+
+// -------------------------------- summary --------------------------------
+
+// 4 new things:
+
+// 1) object.create() -> object banata ha is key argument main wo obj pass kartey hain jo is object key prototype main add karna ha.
+
+// 2) .call() ye sirf function ko call karwata ha or iskey first argument main us object ka name likhtey hain jis ke sath is fuunction key this ki binding karwani ho.
+
+// 3) .apply() ye bhi same .call() ki tarah ha bus function ki properties ko array main likhtey hain.
+
+// 4) .bind() ye aik function return karta ha phir usko call kar saktey hain is key ilawa ye bhi this ki binding key liye use hota ha.
